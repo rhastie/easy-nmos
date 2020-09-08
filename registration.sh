67005e03d3768a37bfaa946829a31d54a4f32c13
Richard Hastie <richh@mellanox.com>
2020-09-08 13:49:49 +0100

    Added Registration API control using registration.sh script and also exposed .local hostnames

---
+++ registration.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+
+CMD=${1:-enable}
+
+enable () {
+  echo "Attempting to enable registration API on nmos-registry"
+
+  curl http://nmos-registry.local:8010/settings/all -X PATCH -H "Content-Type: application/json" -d "{\"registration_available\":true}"
+  if [ $? -eq 0 ]; then
+    echo -e "\nOK - Successfully enabled"
+  else
+    echo -e "\nFAIL - Is the nmos-registry running?"
+  fi
+}
+
+disable () {
+  echo "Attempting to disable registration API on nmos-registry"
+
+  curl http://nmos-registry.local:8010/settings/all -X PATCH -H "Content-Type: application/json" -d "{\"registration_available\":false}"
+  if [ $? -eq 0 ]; then
+    echo -e "\nOK - Successfully disabled"
+  else
+    echo -e "\nFAIL - Is the nmos-registry running?"
+  fi
+}
+
+case $CMD in
+  enable)
+    enable
+    ;;
+  disable)
+    disable
+    ;;
+  *)
+    echo "registration.sh [enable|disable]"
+    ;;
+esac
